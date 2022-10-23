21d83f3a6e97447205686b3f2d098ce5ff6dcf7f
jaeeunjeong <jejeong000@gmail.com>
2022-10-23 20:16:10 +0900

    style : v1 수정 및 주석 추가

--- src/main/java/hello/proxy/ProxyApplication.java
+++ src/main/java/hello/proxy/ProxyApplication.java
@@ -3,15 +3,16 @@ package hello.proxy;
 import hello.proxy.config.AppV1Config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Import(AppV1Config.class)
-@SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의
+// 주의 : 컴포넌트 스캔을 시작할 시작 위치를 정하는 것!
+// -> 시작 위치를 비롯한 그 하위 위치가 아니라면 스프링 빈으로 등록되지 않음.
+@SpringBootApplication(scanBasePackages = "hello.proxy.app")
 public class ProxyApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProxyApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ProxyApplication.class, args);
+    }
 
 }
--- src/main/java/hello/proxy/app/v1/OrderControllerV1.java
+++ src/main/java/hello/proxy/app/v1/OrderControllerV1.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-@RequestMapping// (requsetMapping이나 controoler있으면 인식함.)
+@RequestMapping// (requsetMapping이나 controller가 있어야 controller로 인식함.)
 @ResponseBody
 public interface OrderControllerV1 {
 
