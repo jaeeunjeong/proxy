fb0b1b7b80b2eb5faff56fc3aa50215dce1eecc8
jaeeunjeong <jejeong000@gmail.com>
2022-10-25 23:26:21 +0900

    add : 프록시 객체를 이용하여 캐시를 적용
    프록시 객체가 없다면, 실제 객체를 생성.
    프록시 객체가 있다면, 있는 객체를 사용함.
    
    싱글턴패턴과 비슷하다고 생각이 드는데, 싱글턴 패턴은 객체 하나를 생성해서 계속해서 재사용하는 것이고(객체 자체에 초점을 준 거 같음), 프록시 패턴은 객체를 만들어서 재사용하는 데 그 수명이 짧고, 객체를 만들어서 사용하는 행위에 초첨을 둔 것 같음.

--- src/test/java/hello/proxy/pureproxy/proxy/code/ProxyPatternTest.java
+++ src/test/java/hello/proxy/pureproxy/proxy/code/ProxyPatternTest.java
@@ -13,4 +13,15 @@ public class ProxyPatternTest {
         client.execute();
         client.execute();
     }
+
+    @Test
+    @DisplayName("프록시 적용 후")
+    void test2() {
+        RealSubject realSubject = new RealSubject();
+        CacheProxy proxy = new CacheProxy(realSubject);
+        ProxyPatternClient client = new ProxyPatternClient(proxy);
+        client.execute();
+        client.execute();
+        client.execute();
+    }
 }
