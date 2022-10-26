11ef5954340651e7287edf61fde310dd7c5be6ca
jaeeunjeong <jejeong000@gmail.com>
2022-10-26 22:32:56 +0900

    add : 인터페이스 기반 프록시 적용
    프록시 구현체를 만들고, 그 구현체를 빈에 등록해서 실제 객체 대신 프록시 객체가 주입되어서 사용
    
    프록시 객체를 빈으로 등록해서 사용하는데, 프록시 객체는 실제 객체를 참조하고 있음! -> 실제 객체가 프록시로 감싸진 느낌 또는 실제 객체를 사용하기 위해 프록시를 거쳐가야하는 느낌임.

--- src/main/java/hello/proxy/ProxyApplication.java
+++ src/main/java/hello/proxy/ProxyApplication.java
@@ -1,12 +1,15 @@
 package hello.proxy;
 
-import hello.proxy.config.AppV1Config;
-import hello.proxy.config.AppV2Config;
+import hello.proxy.config.v1_proxy.InterfaceProxyConfig;
+import hello.proxy.trace.logtrace.LogTrace;
+import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 
-@Import({AppV1Config.class, AppV2Config.class})
+//@Import({AppV1Config.class, AppV2Config.class})
+@Import(InterfaceProxyConfig.class)
 // 주의 : 컴포넌트 스캔을 시작할 시작 위치를 정하는 것!
 // -> 시작 위치를 비롯한 그 하위 위치가 아니라면 스프링 빈으로 등록되지 않음.
 @SpringBootApplication(scanBasePackages = "hello.proxy.app")
@@ -16,4 +19,8 @@ public class ProxyApplication {
         SpringApplication.run(ProxyApplication.class, args);
     }
 
+    @Bean
+    public LogTrace logTrace() {
+        return new ThreadLocalLogTrace();
+    }
 }
