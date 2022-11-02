41ab9a5948e49ffacb430172bcf5e813deb8c389
jaeeunjeong <jejeong000@gmail.com>
2022-11-03 01:53:05 +0900

    add : JDK 동적 프록시 적용 + 필터가 필요한 경우

--- src/main/java/hello/proxy/ProxyApplication.java
+++ src/main/java/hello/proxy/ProxyApplication.java
@@ -1,7 +1,6 @@
 package hello.proxy;
 
-import hello.proxy.config.v1_proxy.ConcreteProxyConfig;
-import hello.proxy.config.v2_dynamicproxy.DynamicProxyBasicConfig;
+import hello.proxy.config.v2_dynamicproxy.DynamicProxyFilterConfig;
 import hello.proxy.trace.logtrace.LogTrace;
 import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
@@ -12,7 +11,8 @@ import org.springframework.context.annotation.Import;
 //@Import({AppV1Config.class, AppV2Config.class})
 //@Import(InterfaceProxyConfig.class)
 //@Import(ConcreteProxyConfig.class)
-@Import(DynamicProxyBasicConfig.class)
+//@Import(DynamicProxyBasicConfig.class)
+@Import(DynamicProxyFilterConfig.class)
 // 주의 : 컴포넌트 스캔을 시작할 시작 위치를 정하는 것!
 // -> 시작 위치를 비롯한 그 하위 위치가 아니라면 스프링 빈으로 등록되지 않음.
 @SpringBootApplication(scanBasePackages = "hello.proxy.app")
